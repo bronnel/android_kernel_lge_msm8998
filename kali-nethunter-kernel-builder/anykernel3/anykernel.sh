7768fba4fa51ba786f6ce8d597c95392300244c8
bronnel <austin.316scsa@yahoo.com>
2024-12-31 09:33:52 +0300

    Adapting anykernel builder for LG V30

--- kali-nethunter-kernel-builder/anykernel3/anykernel.sh
+++ kali-nethunter-kernel-builder/anykernel3/anykernel.sh
@@ -6,33 +6,22 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NetHunter Kernel for the OnePlus 7 Pro
+kernel.string=NetHunter Kernel for LG V30
 do.devicecheck=1
 do.modules=1
 do.systemless=0 #Never use this for NetHunter kernels as it prevents us from writing to /lib/modules
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=OnePlus7Pro
-device.name2=guacamole
-device.name3=OnePlus 7 Pro
-device.name4=Guacamole
-device.name5=OnePlus7
-device.name6=guacamoleb
-device.name7=OnePlus 7
-device.name8=Guacamoleb
-device.name9=OnePlus7ProNR
-device.name10=OnePlus7ProTMO
-device.name11=hotdogb
-device.name12=hotdog
-device.name13=OnePlus7T
-device.name14=OnePlus7TPro
+device.name1=joan
+device.name2=LG V30
+device.name3=H930
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
+block=/dev/block/platform/soc/1da4000.ufshc/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
