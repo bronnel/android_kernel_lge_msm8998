fab65c042685f662647d1fca945890c9a6710bb9
bronnel <austin.316scsa@yahoo.com>
2024-12-31 09:29:17 +0300

    Setting lineageos_joan_defconfig and making a test kernel

---
+++ kali-nethunter-kernel-builder/out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Dec 31 09:28:25 +03 2024"
+#define LINUX_COMPILE_BY "matrix"
+#define LINUX_COMPILE_HOST "matrix-GS63VR-6RF"
+#define LINUX_COMPILER "Android (6207600 based on r377782b) clang version 10.0.4 (https://android.googlesource.com/toolchain/llvm-project 0cb1afdb060d6619a9837ab6866cf4adf2336416)"
