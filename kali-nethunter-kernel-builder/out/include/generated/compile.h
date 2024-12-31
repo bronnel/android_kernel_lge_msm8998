68c7013746191d07f0cbee901038c8814fd6a2bc
bronnel <austin.316scsa@yahoo.com>
2024-12-31 11:04:08 +0300

    Configuring the Kernel - NFS

--- kali-nethunter-kernel-builder/out/include/generated/compile.h
+++ kali-nethunter-kernel-builder/out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 8 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#7 SMP PREEMPT Tue Dec 31 10:58:43 +03 2024"
+#define UTS_VERSION "#8 SMP PREEMPT Tue Dec 31 11:03:20 +03 2024"
 #define LINUX_COMPILE_BY "matrix"
 #define LINUX_COMPILE_HOST "matrix-GS63VR-6RF"
 #define LINUX_COMPILER "Android (6207600 based on r377782b) clang version 10.0.4 (https://android.googlesource.com/toolchain/llvm-project 0cb1afdb060d6619a9837ab6866cf4adf2336416)"
